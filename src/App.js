b0317588b76d14bf9577bb03ded7056c79f4b6ce
Gael Mahe <gaelmahe@MacBook-Pro-4.local>
2022-05-20 16:23:14 +0200

    logo image has been fixed

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from "react"
-import Navbar from "./components/Navbar"
-import Main from "./components/Main"
+import Navbar from "./Navbar"
+import Main from "./Main"
 import "./style.css"
 
 export default function App() {
