0fbab869e0f5e828e6098b64d56342ecdceaf221
Gael Mahe <gaelmahe@macbook-pro-4.home>
2022-05-24 12:10:25 +0200

    Card done

---
+++ src/Footer.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+export default function Footer() {
+    return (
+        <div className="footer">
+            <div className="images">
+        <img src="./images/Twitter.png"/>
+        <img src="./images/Facebook.png"/>
+        <img src="./images/Instagram.png"/>
+        <img src="./images/GitHub.png"/>
+        </div>
+        </div>
+    )
+}
